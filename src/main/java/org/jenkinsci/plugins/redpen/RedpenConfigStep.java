5ac2eb306156ac5e563a970fcc56430d4fc84b01
ayush-Ajmera <ayush.kholiya@ajmerainfotech.com>
2022-01-29 12:21:05 +0530

    Add only new Files

--- src/main/java/org/jenkinsci/plugins/redpen/RedpenConfigStep.java
+++ src/main/java/org/jenkinsci/plugins/redpen/RedpenConfigStep.java
@@ -9,15 +9,24 @@ import hudson.model.Result;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Publisher;
 import hudson.tasks.Recorder;
+import org.apache.commons.io.FileUtils;
 import org.jenkinsci.plugins.redpen.ghpr.GithubPrHelper;
 import org.jenkinsci.plugins.redpen.jwt.JWTUtility;
 import org.jenkinsci.plugins.redpen.redpenservices.RedpenService;
 import org.jenkinsci.plugins.redpen.secrets.SecretRetriever;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class RedpenConfigStep extends Recorder {
@@ -53,8 +62,36 @@ public class RedpenConfigStep extends Recorder {
             if (mayBeKey.isPresent()) {
                 try {
                     String jwtToken = JWTUtility.getJWTToken(mayBeKey.get(), this.serviceConnectionId);
-                    redpenService.addAttachment(build, issueKey, jwtToken, build.getLogFile().getAbsolutePath());
+                    File file = new File(build.getLogFile().getAbsolutePath());
+                    File file2 = new File(redpenService.getNewFile(build, build.getLogFile().getAbsolutePath()));
+                    FileUtils.copyFile(file, file2);
+
+                    redpenService.addAttachment(build, issueKey, jwtToken, file2);
                     redpenService.addComment(build, issueKey, jwtToken);
+
+                    String filePath = "/home/ayush/Documents/RedpenWorkspace/Jankins/redpen-jenkins-integration/work/workspace/Jenkins Test Project React/src/e2e/logs";
+
+                    File e2eLogs = new File(filePath);
+
+                    List<File> files = listFilesForFolder(e2eLogs);
+                    for (File file1 : files) {
+                        String absolutePath = file1.getAbsolutePath();
+
+                        Path filePathAbsolute = Paths.get(absolutePath);
+                        BasicFileAttributes attr = Files.readAttributes(filePathAbsolute, BasicFileAttributes.class);
+                        FileTime fileTime = attr.creationTime();
+
+                        FileTime from = FileTime.from(build.getTime().toInstant());
+
+                        int after = from.compareTo(fileTime);
+
+                        if (after == 1) {
+                            redpenService.addAttachment(build, issueKey, jwtToken, file1);
+                        }
+                    }
+
+                    return true;
+
                 } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
                     e.printStackTrace();
                 }
@@ -62,6 +99,22 @@ public class RedpenConfigStep extends Recorder {
             return true;
         }
         return true;
+    }
+
+    public List<File> listFilesForFolder(File folder) {
+        List<File> files = new ArrayList<>();
+        File[] files1 = folder.listFiles();
+        if (folder.exists() && files1 != null) {
+
+            for (File fileEntry : files1) {
+                if (fileEntry.isDirectory()) {
+                    files.addAll(listFilesForFolder(fileEntry));
+                } else {
+                    files.add(fileEntry);
+                }
+            }
+        }
+        return files;
     }
 
     @Extension
--- src/main/java/org/jenkinsci/plugins/redpen/redpenservices/RedpenService.java
+++ src/main/java/org/jenkinsci/plugins/redpen/redpenservices/RedpenService.java
@@ -23,7 +23,7 @@ public class RedpenService {
 
     private static RedpenService instance;
     private final CloseableHttpClient httpClient;
-    private static final String BASE_PATH = "http://localhost:8081";
+    private static final String BASE_PATH = "https://api.dev.redpen.work";
 
     private RedpenService() {
         this.httpClient = HttpClients.createDefault();
@@ -36,49 +36,47 @@ public class RedpenService {
         return instance;
     }
 
-    private String getNewFile(AbstractBuild<?, ?> build, String path) {
+    public String getNewFile(AbstractBuild<?, ?> build, String path) {
         return path + "_" + build.getDisplayName() + "_" + build.getResult();
     }
 
-    public CloseableHttpResponse addAttachment(AbstractBuild<?, ?> build, String issueKey, String token, String path)
+    public void addAttachment(AbstractBuild<?, ?> build, String issueKey, String token, File file)
             throws IOException {
+        try {
+            HttpPost post = new HttpPost(
+                    String.format("%s/external/jenkins/issues/%s/attachments", BASE_PATH, issueKey));
+            FileBody fileBody = new FileBody(file, ContentType.DEFAULT_BINARY);
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            builder.addPart("file", fileBody);
+            HttpEntity entity = builder.build();
 
-        File file = new File(path);
-        File file2 = new File(getNewFile(build, path));
-        FileUtils.copyFile(file, file2);
-        HttpPost post = new HttpPost(String.format("%s/external/jenkins/issues/%s/attachments", BASE_PATH, issueKey));
-        FileBody fileBody = new FileBody(file2, ContentType.DEFAULT_BINARY);
-        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-        builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
-        builder.addPart("file", fileBody);
-        HttpEntity entity = builder.build();
+            post.addHeader("Authorization", "JWT " + token);
+            post.setEntity(entity);
 
-        post.addHeader("Authorization", "JWT " + token);
-        post.setEntity(entity);
-
-        return this.httpClient.execute(post);
+            try (CloseableHttpResponse response = this.httpClient.execute(post)) {
+                EntityUtils.toString(response.getEntity());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void addComment(AbstractBuild<?, ?> build, String issueKey, String token) {
 
         try {
+            String logFileName = getNewFile(build, build.getLogFile().getName());
+            String comment = String.format("Build %s Result %s", build.getDisplayName(), build.getResult());
+            String commentBody = String.format("{ \"comment\": \"[^%s] %s\" }", logFileName, comment);
 
-            String comment = String.format("Build %s", build.getDisplayName());
-            StringBuilder json = new StringBuilder();
-
-            json.append("{ \"comment\": \"").append("[^").append(getNewFile(build, build.getLogFile().getName()))
-                    .append("] ").append(comment).append("\" }");
-
-            HttpPost request = new HttpPost(String.format("%s/external/jenkins/issues/%s/comment", BASE_PATH,
-                    issueKey));
+            HttpPost request = new HttpPost(
+                    String.format("%s/external/jenkins/issues/%s/comment", BASE_PATH, issueKey));
             request.addHeader("Authorization", "JWT " + token);
             request.addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-
-            StringEntity stringEntity = new StringEntity(json.toString());
+            StringEntity stringEntity = new StringEntity(commentBody);
             request.setEntity(stringEntity);
 
             try (CloseableHttpResponse response = this.httpClient.execute(request)) {
-
                 EntityUtils.toString(response.getEntity());
             }
         } catch (IOException e) {
